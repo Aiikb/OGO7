a25423702128afa56f0b9c11667a897182366f29
Em-R2019 <57901428+Em-R2019@users.noreply.github.com>
2019-11-26 15:15:48 +0100

    Update UnpackCell.R

--- Data/UnpackCell.R
+++ Data/UnpackCell.R
@@ -1,10 +1,10 @@
 library(oligo)
 setwd("/Users/20172805/Documents/BMT3/OGO comp/data") # set working directory to datafolder
 celFiles <- list.celfiles()     				# open celfiles to celFiles
-affyRaw <- read.celfiles(celFiles)				# unpack celfiles
-eset <- rma(affyRaw)						# normalise data
+affyRaw <- read.celfiles(celFiles)				# unpack celfiles to genefeatureset
+eset <- rma(affyRaw)						# normalise data and save as expressionset
 write.exprs(eset,file="data.txt")				# write to file and log2 transform
 
 
-library(lumi)
-lumiR("Raw_data_matrix_file.txt", sep = NULL) 
+library(lumi)                           
+lumiRaw <- lumiR("Raw_data_matrix_file.txt", sep = NULL)   # Read lumi matrix file to lumibatch file
